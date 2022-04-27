2301b220040d71226436975fb7bc1af0af545eb5
IACPouembout <jonas.brouillon@iac.nc>
2022-04-28 10:53:34 +1100

    MàJ

--- scripts/cours_8_manipulation_texte.R
+++ scripts/cours_8_manipulation_texte.R
@@ -1,4 +1,4 @@
-#######################Cours 7 : manipuler du texte##############################################################################
+#######################Cours 8 : manipuler du texte##############################################################################
 
 
 pacman::p_load("tidyverse",
@@ -17,16 +17,20 @@ d <- tibble(
 
 
 ####################################################### Concaténation #######################################################
+#paste pour concatener 2 caracteres, sep= pour choisir le separateur
 
 paste(d$adresse, d$ville)
 
 paste(d$adresse, d$ville, sep = " - ")
 
+#paste0 pour une concatenation sans espace
 paste0(d$adresse, d$ville)
 
+#on peut aussi concatener l'ensemble des catacteres d'un vecteur
 paste(d$ville, collapse = ", ")
 ####################################################### Convertir en majuscule/minuscule #######################################################
 
+#package stringr
 str_to_lower(d$nom)
 
 str_to_upper(d$nom)
@@ -35,6 +39,7 @@ str_to_title(d$nom)
 
 str_to_sentence(d$nom)
 
+#package snakecase
 to_snake_case(d$nom)
 
 to_lower_upper_case(d$nom)
@@ -44,19 +49,21 @@ to_lower_camel_case(d$nom)
 to_screaming_snake_case(d$nom)
 ####################################################### Découper des chaines #######################################################
 
+#stringr
 str_split("un-deux-trois", "-")
 
 str_split(d$nom, " ")
 
 str_split(d$nom, " ", simplify = TRUE)
 
+#tidyr
 d%>%
   separate(nom,c("genre", "prenom", "nom"),sep=" ")
 
 
 #######################################################Extraire des sous chaines par position #######################################################
 
-
+#str_sub(vecteur,debut,fin)
 str_sub(d$ville, 1, 3)
 
 
@@ -113,3 +120,15 @@ str_replace_all(
   c("avenue" = "Avenue", "ave" = "Avenue", "rue" = "Rue")
 )
 
+####################################################### Exercices #######################################################
+
+d <- tibble(
+  nom = c("M. rené Bézigue", "Mme Paulette fouchin", "Mme yvonne duluc", "M. Jean-Yves Pernoud"),
+  naissance = c("18/04/1937 Vesoul", "En 1947 à Grenoble (38)", "Le 5 mars 1931 à Bar-le-Duc", "Marseille, juin 1938"),
+  profession = c("Ouvrier agric", "ouvrière qualifiée", "Institutrice", "Exploitant agric")
+)
+
+# 1 Capitalisez les noms des personnes avec str_to_title 
+# 2 Dans la variable profession, remplacer toutes les occurrences de l’abbréviation “agric” par “agricole” 
+# 3 À l’aide de str_detect, identifier les personnes de catégorie professionnelle “Ouvrier”. Indication : pensez au modificateur ignore_case.
+# 4 À l’aide de case_when et de str_detect, créer une nouvelle variable sexe identifiant le sexe de chaque personne en fonction de la présence de M. ou de Mme dans son nom.
